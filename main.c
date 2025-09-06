0322e168e459374b29245a36d283d1d12315cc72
sermontoya <sermontoya@estudiantec.cr>
2025-09-06 12:34:15 -0600

    menú corregido

--- main.c
+++ main.c
@@ -3,36 +3,58 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+void admin(){
+    printf("Estamos en admin");
+    
+}
+
+void general(){
+    printf("Estamos en general");
+}
 
 
+
+void limpiarBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
 
 int main() {
-    printf("Bienvenido al sistema de Librería\n");
-    printf("Menu principal:\n");
-    printf("1- Opciones Administrativas\n");
-    printf("2- Opciones Generales\n");
-    printf("3- Salir\n");
-    printf("Seleccione una opción: ");
-    int respuesta;
-    scanf("%d", &respuesta);
-    if(respuesta == 1) {
-    
-    } else if(respuesta == 2) {
-
-    } else if(respuesta == 3) {
-        printf("¿Estás seguro que deseas salir (Y/N):");
-        char *res;
-        scanf("%s", res);
-        if(!strcmp(res, "n") || !strcmp(res, "N")){ main();}
-        else{
-            exit(0);
+    while(true){
+        printf("Bienvenido al sistema de Librería\n");
+        printf("Menu principal:\n");
+        printf("1- Opciones Administrativas\n");
+        printf("2- Opciones Generales\n");
+        printf("3- Salir\n");
+        printf("Seleccione una opción: ");
+        int respuesta;
+        if(scanf("%d", &respuesta)==1){
+            if(respuesta == 1) {
+            admin();
+            } else if(respuesta == 2) {
+                general();
+            } else if(respuesta == 3) {
+                printf("¿Estás seguro que deseas salir (Y/N):");
+                char *res;
+                scanf("%s", res);
+                if(!strcmp(res, "n") || !strcmp(res, "N")){ main();}
+                else{
+                    exit(0);
+                }
+            } else {
+                printf("Opción no válida. Por favor ingrese una de las opciones.\n");
+                main();
+            }
+        }else{
+            printf("Entrada incorrecta. Por favor, ingresa un número\n");
+            limpiarBuffer();
         }
-    } else {
-        printf("Opción no válida. Por favor ingrese una de las opciones.\n");
-        main();
+        
     }
+    
     return 0;
 
-
-
 }
+
